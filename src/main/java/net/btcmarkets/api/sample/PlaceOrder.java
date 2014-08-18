0d07ef7e7dcd0a3d47359e7c0fff4d30bb2aa4d5
Daniel Vogel <dvogel@cs.stanford.edu>
2014-08-18 11:05:31 -0400

    now on to getting my account's balance

--- src/main/java/net/btcmarkets/api/sample/PlaceOrder.java
+++ src/main/java/net/btcmarkets/api/sample/PlaceOrder.java
@@ -169,5 +169,6 @@ public class PlaceOrder {
                 "/order/create",
                 "{\"currency\":\"AUD\",\"instrument\":\"BTC\",\"price\":13000000000,\"volume\":10000000,\"orderSide\":\"Bid\",\"ordertype\":\"Limit\",\"clientRequestId\":\"1\"}");
         sendRequest("/order/history", "{\"currency\":\"AUD\",\"instrument\":\"BTC\",\"limit\":10,\"since\":1}");
+        sendRequest("/account/balance", null);
     }
 }
